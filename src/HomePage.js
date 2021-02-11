404453ccdb5161e6145da846fb201252677e6534
IrinaKuz <ira.kuznets@gmail.com>
2021-02-10 22:49:10 -0500

    Revised styles

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,13 +5,16 @@ function load_home(container) {
         container.innerText = '';
     }
     const header = document.createElement('header');
+    const p_welcome = document.createElement('p');
+    p_welcome.innerHTML = 'Welcome to'
     const h1 = document.createElement('h1');
-    h1.innerText = 'The Breakfast Club';
+    h1.innerHTML = '<span class="emp">The Breakfast Club<span>';
     const p = document.createElement('p');
     p.innerText = 'Best breakfast in town!';
     const img = document.createElement('img');
     img.setAttribute('src', './img/english_breakfast.jpg');
     img.setAttribute('alt', 'English breakfast');
+    header.appendChild(p_welcome);
     header.appendChild(h1);
     header.appendChild(p);
     header.appendChild(img);
